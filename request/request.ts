749c1c964de1184562b139cf3bba5997c2af8c0d
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-10-01 16:26:15 -0300

    add request module
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

---
+++ request/request.ts
@@ -0,0 +1,12 @@
+// run npm install --save-dev typescript && npx tsc --version && npx tsc request.ts to generate JAVASCRIPT file
+const request = require('request');
+
+console.log("START")
+
+request('https://v3.ritchiecli.io/stable.txt', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+console.log("END")
